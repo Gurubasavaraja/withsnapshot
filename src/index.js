3e4df697cd7e273be2d35b5598ecb1e08a9b647b
Guru081 <gurubasavaraj1996@gmail.com>
2019-10-24 16:46:19 +0530

    ss

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
+import App from './App';
+import './index.css';
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+render(
+  <App />,
+  document.getElementById('root')
+);
